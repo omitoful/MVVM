f2a558614c3d99747a305576730218bf0d13cfd0
Kevin <kevin@twjoin.com>
2023-09-14 16:39:12 +0800

    setup event & use viewmodel in view controller

--- MVVM-ex/Screens/Product/View/ProductListViewController.swift
+++ MVVM-ex/Screens/Product/View/ProductListViewController.swift
@@ -8,14 +8,36 @@
 import UIKit
 
 class ProductListViewController: UIViewController {
+    
+    private var viewModel = ProductViewModel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        APIManager.shared.fetchProducts { response in
-            switch response {
-            case .success(let products):
-                print(products)
-            case .failure(let error):
+        configuration()
+    }
+}
+
+extension ProductListViewController {
+    
+    func configuration() {
+        initViewModel()
+        observeEvent()
+    }
+    
+    func initViewModel() {
+        viewModel.fetchProduct()
+    }
+    
+    func observeEvent() {
+        viewModel.eventHandler = { [weak self] event in
+            guard let self else { return }
+            
+            switch event {
+            case .loading: break
+            case .stopLoading: break
+            case .dataLoaded:
+                print(self.viewModel.products)
+            case .error(let error):
                 print(error)
             }
         }
--- MVVM-ex/Screens/Product/ViewModel/ProductViewModel.swift
+++ MVVM-ex/Screens/Product/ViewModel/ProductViewModel.swift
@@ -13,13 +13,25 @@ final class ProductViewModel {
     var eventHandler: ((_ event: Event) -> Void)? // Data Binding Closure
     
     func fetchProduct() {
+        self.eventHandler?(.loading)
         APIManager.shared.fetchProducts { response in
+            self.eventHandler?(.stopLoading)
             switch response {
             case .success(let products):
                 self.products = products
+                self.eventHandler?(.dataLoaded)
             case .failure(let error):
-                print(error)
+                self.eventHandler?(.error(error))
             }
         }
+    }
+}
+
+extension ProductViewModel {
+    enum Event {
+        case loading
+        case stopLoading
+        case dataLoaded
+        case error(Error?)
     }
 }
