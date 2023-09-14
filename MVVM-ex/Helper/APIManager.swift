7f45dbacc9d8e43d566155fa826f8cb8024cacb7
Kevin <kevin@twjoin.com>
2023-09-14 15:21:08 +0800

    setup api error

--- MVVM-ex/Helper/APIManager.swift
+++ MVVM-ex/Helper/APIManager.swift
@@ -7,24 +7,40 @@
 
 import UIKit
 
+enum DataError: Error {
+    case invalidResponse
+    case invalidURL
+    case invalidData
+    case network(Error?)
+}
+
+typealias Handler = (Result<[Product], DataError>) -> Void
+
 class APIManager {
     
     static let shared = APIManager()
     private init() {}
     
-    func fetchProducts() {
+    func fetchProducts(completion: @escaping Handler) {
         guard let url = URL(string: Constant.API.productURL) else { return }
         
         URLSession.shared.dataTask(with: url) { data, response, error in
-            guard let data, error == nil else { return }
+            guard let data, error == nil else {
+                completion(.failure(.invalidData))
+                return
+            }
             guard let response = response as? HTTPURLResponse,
-                  200 ... 299 ~= response.statusCode else { return }
+                  200 ... 299 ~= response.statusCode else {
+                completion(.failure(.invalidResponse))
+                return
+            }
             
             do {
-                let product = try JSONDecoder().decode(Product.self, from: data)
+                let products = try JSONDecoder().decode([Product].self, from: data)
+                completion(.success(products))
                 
             } catch {
-                
+                completion(.failure(.network(error)))
             }
             
         }.resume()
